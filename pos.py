17d8a38ac04ac474e4ae712a091d6fe636b6cde3
Nagendranmedisetty <40906709+Nagendranmedisetty@users.noreply.github.com>
2018-07-12 11:55:34 +0530

    Update pos.py

--- pos.py
+++ pos.py
@@ -1,7 +1,7 @@
-num = float(input("Enter a number: "))
+num = float(input("Enter the number: "))
 if num > 0:
-   print("Positive number")
+print("Positive")
 elif num == 0:
-   print("Zero")
+print("Zero")
 else:
-   print("Negative number")
+print("Negative")
