d36fd8562fb8b7ff782f71a6797991295093d1cf
Nagendranmedisetty <40906709+Nagendranmedisetty@users.noreply.github.com>
2018-07-18 11:41:32 +0530

    Update odd.py

--- odd.py
+++ odd.py
@@ -1,5 +1,5 @@
-lower=int(input("Enter the lower limit for the range:"))
-upper=int(input("Enter the upper limit for the range:"))
-for i in range(lower,upper+1):
+l=int(input("Enter the lower limit range:"))
+u=int(input("Enter the upper limit range:"))
+for i in range(l,u+1):
     if(i%2!=0):
         print(i)
